6b49f741b5f66acd09e963a3f12a40466b81bf85
choi-yuna <mjung0226@naver.com>
2024-03-25 23:30:24 +0900

    상단 바 날짜 구현

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { runPracticeDayjs } from './src/practice-dayjs';
 import { getCalendarColumns, getDayColor, getDayText} from './src/util';
 import dayjs from 'dayjs';
+import { getCalendarColumns, getDayColor, getDayText } from './src/util';
+import { SimpleLineIcons } from '@expo/vector-icons'; 
 
 //날짜, 요일 column style 함수
 const columnSize =35;
@@ -32,15 +34,34 @@ export default function App() {
 
   //상단 헤더 함수
   const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format("YYYY.MM.DD");
     return (
       <View>
+        <Mergin height={15} />
+        {/* <YYYY.MM.DD/> */}
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems:"centee"}}>
+          <TouchableOpacity>
+            <SimpleLineIcons iconName="arrow-left" size={15} color="#404040" />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+          <Text style={{ fontSize: 20, color: "#404040"}}> {currentDateText}</Text>
+          </TouchableOpacity> 
+
+          <TouchableOpacity>
+          <SimpleLineIcons iconName="arrow-right" size={15} color="#404040" />
+          </TouchableOpacity>
+
+        </View>
+        <Mergin height={15} />
+        {/*월 화 수 목 금 토 일 */}
         <View style={{ flexDirection: "row" }}>
           {[0, 1, 2, 3, 4, 5, 6].map(day => {
             const dayText = getDayText(day);
             const color = getDayColor(day);
             return (
               <Column key={day} text={dayText} color={color} opacity={1} />
-            )
+            ) 
           })}
         </View>
       </View>
