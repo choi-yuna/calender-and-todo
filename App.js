2378e4e4ec15859338b236245ad3ba0e8cb960fb
choi-yuna <mjung0226@naver.com>
2024-03-26 23:04:43 +0900

    Feat: CalenderView 구현

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from
 import { runPracticeDayjs } from './src/practice-dayjs';
 import { getCalendarColumns, getDayColor, getDayText} from './src/util';
 import dayjs from 'dayjs';
-import { getCalendarColumns, getDayColor, getDayText } from './src/util';
 import { SimpleLineIcons } from '@expo/vector-icons'; 
+import Margin from './Margin';
 
+// MARK: - Property
 //날짜, 요일 column style 함수
 const columnSize =35;
 
@@ -27,8 +28,17 @@ const Column = ({
  )
 }
 
+const ArrowButton = ({iconName}) => {
+  return(
+    <TouchableOpacity style = {{paddingHorizontal:20, paddingVertical:15}}>
+            <SimpleLineIcons name={iconName} size={15} color="#404040" />
+    </TouchableOpacity>
+  )
+}
+
+
 export default function App() {
-  //속성값들 
+  // MARK: - Property
   const now = dayjs();
   const columns = getCalendarColumns(now);
 
@@ -37,30 +47,27 @@ export default function App() {
     const currentDateText = dayjs(now).format("YYYY.MM.DD");
     return (
       <View>
-        <Mergin height={15} />
-        {/* <YYYY.MM.DD/> */}
-        <View style={{flexDirection: "row", justifyContent: "center", alignItems:"centee"}}>
-          <TouchableOpacity>
-            <SimpleLineIcons iconName="arrow-left" size={15} color="#404040" />
-          </TouchableOpacity>
+
+        {/* <YYYY.MM.DD> */}
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems:"center"}}>
+          <ArrowButton iconName="arrow-left" />
 
           <TouchableOpacity>
           <Text style={{ fontSize: 20, color: "#404040"}}> {currentDateText}</Text>
           </TouchableOpacity> 
 
-          <TouchableOpacity>
-          <SimpleLineIcons iconName="arrow-right" size={15} color="#404040" />
-          </TouchableOpacity>
+          <ArrowButton iconName="arrow-right" />
 
         </View>
-        <Mergin height={15} />
-        {/*월 화 수 목 금 토 일 */}
+
+
+        {/*일 월 화 수 목 금 토 */}
         <View style={{ flexDirection: "row" }}>
           {[0, 1, 2, 3, 4, 5, 6].map(day => {
             const dayText = getDayText(day);
             const color = getDayColor(day);
             return (
-              <Column key={day} text={dayText} color={color} opacity={1} />
+              <Column key={`day-${day}`} text={dayText} color={color} opacity={1} />
             ) 
           })}
         </View>
@@ -69,7 +76,7 @@ export default function App() {
   }
 
 
-  // 날짜 item 가져오는 함수 
+  // Fuction날짜 item 가져오는 함수 
   const renderItem = ({ item: date }) => {
     const dateText = dayjs(date).get('date');
     const day = dayjs(date).get('day');
@@ -86,11 +93,13 @@ export default function App() {
     runPracticeDayjs();
   }, []);
 
-  //리턴 값
+
+  // MARK: - Body
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data = {columns}
+        keyExtractor={(_, index) => `column-${index}`}
         numColumns={7}
         renderItem={renderItem}
         ListHeaderComponent={ListHeaderComponent}
@@ -98,6 +107,7 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
